4e724a7baaac71fd0da7ddddfc9d13ea4941e956
Kate Finlay <kate.n.finlay@gmail.com>
2014-09-05 09:55:24 -0700

    Commit page 60

--- BullsEye/BullsEye/KFViewController.m
+++ BullsEye/BullsEye/KFViewController.m
@@ -15,12 +15,14 @@
 @implementation KFViewController
 {
     int _currentValue;
+    int _targetValue;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	_currentValue = self.slider.value;
+    _targetValue = 1 + arc4random_uniform(100);
 }
 
 - (void)didReceiveMemoryWarning
@@ -32,12 +34,13 @@
 - (IBAction)showAlert
 {
     NSString *message = [NSString stringWithFormat:
-      @"The value of the slider is: %d", _currentValue];
+      @"The value of the slider is: %d\nThe target value is: %d", _currentValue, _targetValue];
     
     UIAlertView *alertView = [[UIAlertView alloc]
       initWithTitle:@"Hello, World!" message:message
       delegate:nil
-      cancelButtonTitle:@"OK" otherButtonTitles:nil];
+      cancelButtonTitle:@"OK"
+      otherButtonTitles:nil];
     
     [alertView show];
 }
