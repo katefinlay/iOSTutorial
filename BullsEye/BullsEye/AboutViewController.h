310ce873490c65e4f8c1e2bdb3f680fd44a0f7e1
Kate Finlay <kate.n.finlay@gmail.com>
2014-09-08 21:50:11 -0700

    Added the about screen (page 100)

---
+++ BullsEye/BullsEye/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Katharine Finlay on 9/8/14.
+//  Copyright (c) 2014 Kate Finlay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
