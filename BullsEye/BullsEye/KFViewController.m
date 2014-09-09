42fe8e3fa3ea21c22db8cfc7844eb36ebd76ac7e
Kate Finlay <kate.n.finlay@gmail.com>
2014-09-09 08:47:43 -0700

    Added launch screen, icons, and formatted name (page 132). Done with assignment 2 tutorial.

--- BullsEye/BullsEye/KFViewController.m
+++ BullsEye/BullsEye/KFViewController.m
@@ -114,8 +114,12 @@
 
 - (IBAction)startOver
 {
+    CATransition *transition = [CATransition animation];
+    transition.type = kCATransitionFade; transition.duration = 1;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
     [self startNewGame];
     [self updateLabels];
+    [self.view.layer addAnimation:transition forKey:nil];
 }
 
 - (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
